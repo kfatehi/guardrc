e60de1334a69dbe65bf8a3a4655487137790bf6c
Keyvan <keyvan@brevity>
2012-07-02 10:04:45 -0700

    Improve & version bump

--- lib/guardrc/guardrc.rb
+++ lib/guardrc/guardrc.rb
@@ -1,10 +1,14 @@
 module Guardrc
+  @cache = ''
   def Guardrc.at(path)
+    puts "Guardrc is looking for extra commands at #{path}" if path
     @path = File.expand_path(path)
     if File.exists?(@path)
-      File.open(@path, 'r') { |f| f.read }
+      File.open(@path, 'r') { |f| @cache = f.read }
+      puts "Guardrc has loaded your auxiliary commands!"
     else
-      "puts 'Guard-compatible file not found #{@path}'"
+      puts "Guardrc did not find any file at #{@path}"
     end
+    return @cache
   end
 end
